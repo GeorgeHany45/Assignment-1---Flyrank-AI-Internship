9611973fd4cbc0df498308c7006f24d0bd7b07a9
George Hany <georgehany562002@gmail.com>
2026-07-15 16:23:28 +0300

    Stage 2: read endpoints with 404

--- server.js
+++ server.js
@@ -2,7 +2,26 @@ const express = require ('express')
 
 const app = express()
 
+// tasks array
+const tasks = [
+    {
+        id: 1,
+        title: "Complete Node.js assignment",
+        done: false
+    },
+    {
+        id: 2,
+        title: "Study Express routing",
+        done: true
+    },
+    {
+        id: 3,
+        title: "Push project to GitHub",
+        done: false
+    }
+];
 
+// server routes
 app.get('/', (req, res) => {
     res.json({
         "name": "Task API",
@@ -16,6 +35,23 @@ app.get('/health', (req, res) => {
         status : 'ok'
     });
 });
+
+//task routes
+app.get('/tasks', (req,res)=>{ 
+    res.status(200).json(tasks)
+})
+
+app.get('/tasks/:id', (req,res)=>{ 
+    const taskid = Number(req.params.id)
+    const taskrequired = tasks.find((t)=>t.id === taskid)
+    if(taskrequired){
+        res.status(200).json(taskrequired)
+    }
+    else{
+        res.status(404).json({error : `task ${taskid} is not found`})
+    }
+})
+
 
 
 app.listen ( 3000 , ()=>{
